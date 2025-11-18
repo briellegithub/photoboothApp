402a96852532e922d9a7e5d3411cd88a399f0f67
briellegithub <briellehwang@gmail.com>
2025-11-18 12:06:12 -0500

    Fixed error

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-const { app, BrowserWindow } = require('electron')
-
+const { app, BrowserWindow } = require('electron/main')
+const path = require('node:path')
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      preload: Path2D.join(__dirname, 'preload.js')
+      preload: path.join(__dirname, 'preload.js')
     }
   })
 
@@ -15,4 +15,16 @@ const createWindow = () => {
 
 app.whenReady().then(() => {
   createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
 })
